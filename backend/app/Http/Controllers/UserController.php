fc7d446efb6b60f3dde2e7daf8e931dc0462bf03
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2024-01-01 02:36:03 +0700

    feat(user): add user registration functionality

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['user' => $user, 'message' => 'User registered successfully'], 201);
+    }
 }
+
