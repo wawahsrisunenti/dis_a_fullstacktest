311bd812ac1519c6224f27b9ee9b66f3b8a57c1a
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2024-01-01 04:29:35 +0700

    feat: create AuthForm.js

---
+++ frontend/src/components/AuthForm.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+
+const AuthForm = ({ onSubmit, buttonText, isRegisterForm }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {isRegisterForm && (
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+      )}
+
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
+
+      {isRegisterForm && (
+        <div>
+          <label htmlFor="password_confirmation">Confirm Password:</label>
+          <input
+            type="password"
+            id="password_confirmation"
+            name="password_confirmation"
+            value={formData.password_confirmation}
+            onChange={handleChange}
+            required
+          />
+        </div>
+      )}
+
+      <div>
+        <button type="submit">{buttonText}</button>
+      </div>
+    </form>
+  );
+};
+
+export default AuthForm;
