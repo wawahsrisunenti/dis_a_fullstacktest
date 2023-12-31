7f7334c8368cfa8eda91d8f4d727c3525d1dffb5
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2024-01-01 03:35:12 +0700

    feat(api): add API routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,19 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Authentication routes
+Route::post('/api/register', [UserController::class, 'register']);
+Route::post('/api/login', [UserController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Protected routes (require authentication)
+Route::middleware('auth:api')->group(function () {
+    Route::post('/api/user/logout', [UserController::class, 'logout']);
+    Route::get('/api/user', [UserController::class, 'index']);
+    Route::get('/api/user/{id}', [UserController::class, 'show']);
+    Route::put('/api/user/{id}', [UserController::class, 'update']);
+    Route::delete('/api/user/{id}', [UserController::class, 'destroy']);
 });
